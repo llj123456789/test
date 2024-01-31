bb260fb9afcd658725ac933167f75a407d635831
liuliangjin <2041584846@qq.com>
2024-01-31 17:51:28 +0800

    vision 3.3

---
+++ coutgitusage.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+
+    cout<<"main 主分支"<<endl;
+    cout<<"git init 初始化"<<endl<<"git add 添加到暂存区"<<endl<<"git status 查看状态"<<endl<<"git commit 提交暂存区 -m “内容” "<<endl;
+    cout<<"git branch 创建新分支"<<endl<<"git switch 转换分支 -c 创建并转换分支"<<endl;
+    cout<<"                "<<"也可以 git checkout 转换分支"<<endl<<"        git checkout -b 创建并转换分支"<<endl;
+    cout<<"git merge 将其他分支合并到当前分支"<<endl;
+    
+}   
